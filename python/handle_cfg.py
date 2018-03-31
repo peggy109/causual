998b271c051cefd727792f14232b56822758e9cb
infoadmin2017 <xuwenxiu@droi.com>
2018-03-31 16:50:08 +0800

    [python] read_cfg.py

--- python/handle_cfg.py
+++ python/handle_cfg.py
@@ -1,38 +1,43 @@
 #!/usr/bin/python
-def get_config_val(cfg,group,key):
-  import ConfigParser
+import os
+import ConfigParser
+def get_config_val(cfg,section,key):
   config=ConfigParser.ConfigParser();
   config.read(cfg)
-  print "has sections:", config.has_section(group)
-  print "sections:",config.sections()
-  print "has options:",config.has_option(group,key)
-  print "options:",config.options(group)
-  return config.get(group,key);
-def place_config(cfg,group,key,value):
-  import ConfigParser
-  config=ConfigParser.ConfigParser();
-  import os
-  if os.path.isfile(cfg):
-      config.read(cfg)
-      if config.has_section(group) == True:
-          if config.has_option(group, key) == True:
-              print "[update]"
-              config.set(group, key, value)
-              config.write(open(cfg, "r+"))
-          else:
-              print "[add option]"
-              config.set(group, key, value)
-              config.write(open(cfg, "r+"))
+  if section == "" or key == "":
+      print "section or key empty"
+      return None;
+  if config.has_section(section) == True:
+      if config.has_option(section, key) == True:
+              print "val:",config.get(section, key,)
+              return config.get(section, key,)
       else:
-          print "[add section]"
-          config.add_section(group)
-          config.set(group, key, value)
-          config.write(open(cfg, "r+"))
+          print "key not exist"
+          return None
   else:
-        print "[new]"
-        config.add_section(group);
-        config.set(group, key, value)
-        config.write(open(cfg, "w"));
+          print "section not exist"
+          return None
+
+def place_config(cfg,group,key,value):
+  config=ConfigParser.ConfigParser();
+  if group == "" or key == "":
+      return
+  config.read(cfg)
+  if config.has_section(group) == True:
+      if config.has_option(group, key) == True:
+          print "[update]"
+          config.set(group, key, value)
+          config.write(open(cfg, "w"))
+      else:
+          print "[add option]"
+          config.set(group, key, value)
+          config.write(open(cfg, "w"))
+  else:
+      print "[add section]"
+      config.add_section(group)
+      config.set(group, key, value)
+      config.write(open(cfg, "w"))
+
 if __name__ == "__main__":
     import sys
     cfg = sys.argv[1];
@@ -40,4 +45,3 @@ if __name__ == "__main__":
     key = sys.argv[3];
     val = sys.argv[4];
     place_config(cfg, group, key, val);
-
